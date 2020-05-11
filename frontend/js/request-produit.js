ac927221297156c20cfee2dfa568d0a0dff63dbd
Olivier Barjon <olivier.barjon@gmail.com>
2020-05-11 12:59:35 +0200

    Création des requêtes de base pour index et produit. Mise en place des parametres d'url window.location

---
+++ frontend/js/request-produit.js
@@ -0,0 +1,62 @@
+
+// récupération des éléments du DOM
+const nomModele = document.getElementById ("nomModele");
+const prixModele = document.getElementById ("prixModele");
+const descripModele = document.getElementById ("descripModele");
+const imageModele = document.getElementById ("imageModele");
+const ButtonModele = document.getElementById ("ButtonModele");
+
+// fonction de récupération du paramètre d'url
+function obtenirParametreUrl () {
+    parametreUrl= window.location.search;
+    const Id = parametreUrl.split("=");
+    return Id[1];
+  }
+// TEST
+//alert (obtenirParametreUrl());
+
+// création d'une variable d'index de Bdd
+let indexBdd;
+// assignation des index de la Bdd en fonction des Id
+switch (obtenirParametreUrl()){
+    case "5be1ed3f1c9d44000030b061" :
+        indexBdd = 0;
+        break;
+    case "5be1ef211c9d44000030b062" :
+        indexBdd = 1;
+        break;
+    case "5be9bc241c9d440000a730e7" :
+        indexBdd = 2;
+        break;
+    case "5be9c4471c9d440000a730e8" :
+        indexBdd = 3;
+        break;
+    case "5be9c4c71c9d440000a730e9" :
+        indexBdd = 4;
+        break;
+}
+  
+
+// REQUETE PAGE PRODUIT
+var request = new XMLHttpRequest(); 
+request.onreadystatechange = function () { 
+    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) { 
+        const response = JSON.parse(this.responseText); 
+
+        nomModele.textContent=`${response[indexBdd].name}`;
+
+        prixModele.textContent=`${response[indexBdd].price}`;
+
+        descripModele.textContent=`${response[indexBdd].description}`;
+
+        imageModele.innerHTML=`<img class="card-img-top card__body--image" src="${response[indexBdd].imageUrl}" alt="${response[indexBdd].name}"/>`;
+
+        ButtonModele.innerHTML=`<button type="button" class="btn btn-lg btn-outline-primary">Ajouter au panier</button>`;
+
+        //TEST
+        console.table(response);
+
+         };
+    }
+request.open("GET","http://localhost:3000/api/cameras");
+request.send();
--- frontend/js/request.js
+++ frontend/js/request.js
@@ -1,65 +1,74 @@
+// récupération des éléments du DOM
 const nomModele1 = document.getElementById ("nomModele1");
 const prixModele1 = document.getElementById ("prixModele1");
 const descripModele1 = document.getElementById ("descripModele1");
 const imageModele1 = document.getElementById ("imageModele1");
-const lensOption1 = document.getElementById ("lensOption1");
-
+const ButtonModele1 = document.getElementById ("ButtonModele1");
 
 const nomModele2 = document.getElementById ("nomModele2");
 const prixModele2 = document.getElementById ("prixModele2");
 const descripModele2 = document.getElementById ("descripModele2");
 const imageModele2 = document.getElementById ("imageModele2");
-const lensOption2 = document.getElementById ("lensOption2");
+const ButtonModele2 = document.getElementById ("ButtonModele2");
 
-const nom = [];
-const prix = [];
-const description = [];
-const image = [];
-const lentilles = [];
-const lentilles1 = [];
-const lentilles2 = [];
+const nomModele3 = document.getElementById ("nomModele3");
+const prixModele3 = document.getElementById ("prixModele3");
+const descripModele3 = document.getElementById ("descripModele3");
+const imageModele3 = document.getElementById ("imageModele3");
+const ButtonModele3 = document.getElementById ("ButtonModele3");
 
+const nomModele4 = document.getElementById ("nomModele4");
+const prixModele4 = document.getElementById ("prixModele4");
+const descripModele4 = document.getElementById ("descripModele4");
+const imageModele4 = document.getElementById ("imageModele4");
+const ButtonModele4 = document.getElementById ("ButtonModele4");
 
+const nomModele5 = document.getElementById ("nomModele5");
+const prixModele5 = document.getElementById ("prixModele5");
+const descripModele5 = document.getElementById ("descripModele5");
+const imageModele5 = document.getElementById ("imageModele5");
+const ButtonModele5 = document.getElementById ("ButtonModele5");
 
+// REQUETE PAGE INDEX
 var request = new XMLHttpRequest(); 
 request.onreadystatechange = function () { 
     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) { 
         const response = JSON.parse(this.responseText); 
 
-        for (let objet of response) {
-            nom.push(objet.name);
-            prix.push(objet.price);
-            description.push(objet.description);
-            image.push(objet.imageUrl);
-            lentilles.push(objet.lenses);
-        }
+        nomModele1.textContent=`${response[0].name}`;
+        nomModele2.textContent=`${response[1].name}`;
+        nomModele3.textContent=`${response[2].name}`;
+        nomModele4.textContent=`${response[3].name}`;
+        nomModele5.textContent=`${response[4].name}`;
 
-        nomModele1.textContent=`${nom[0]}`;
-        nomModele2.textContent=`${nom[1]}`;
+        prixModele1.textContent=`${response[0].price}`;
+        prixModele2.textContent=`${response[1].price}`;
+        prixModele3.textContent=`${response[2].price}`;
+        prixModele4.textContent=`${response[3].price}`;
+        prixModele5.textContent=`${response[4].price}`;
 
-        prixModele1.textContent=prix[0];
-        prixModele2.textContent=prix[1];
+        descripModele1.textContent=`${response[0].description}`;
+        descripModele2.textContent=`${response[1].description}`;
+        descripModele3.textContent=`${response[2].description}`;
+        descripModele4.textContent=`${response[3].description}`;
+        descripModele5.textContent=`${response[4].description}`;
 
-        descripModele1.textContent=description[0];
-        descripModele2.textContent=description[1];
+        imageModele1.innerHTML=`<img class="card-img-top card__body--image" src="${response[0].imageUrl}" alt="${response[0].name}"/>`;
+        imageModele2.innerHTML=`<img class="card-img-top card__body--image" src="${response[1].imageUrl}" alt="${response[1].name}"/>`;
+        imageModele3.innerHTML=`<img class="card-img-top card__body--image" src="${response[2].imageUrl}" alt="${response[2].name}"/>`;
+        imageModele4.innerHTML=`<img class="card-img-top card__body--image" src="${response[3].imageUrl}" alt="${response[3].name}"/>`;
+        imageModele5.innerHTML=`<img class="card-img-top card__body--image" src="${response[4].imageUrl}" alt="${response[4].name}"/>`;
 
-        imageModele1.innerHTML=`<img class="card__body--image" src="${image[0]}" alt="nom[0]"/>`;
-        imageModele2.innerHTML=`<img class="card__body--image" src="${image[1]}" alt="nom[1]"/>`;
+        ButtonModele1.innerHTML=`<a href="produit.html?ID=${response[0]._id}" type="button" class="btn btn-lg btn-outline-primary">Fiche produit du ${response[0].name} </a>`;
+        ButtonModele2.innerHTML=`<a href="produit.html?ID=${response[1]._id}" type="button" class="btn btn-lg btn-outline-primary">Fiche produit du ${response[1].name} </a>`;
+        ButtonModele3.innerHTML=`<a href="produit.html?ID=${response[2]._id}" type="button" class="btn btn-lg btn-outline-primary">Fiche produit du ${response[2].name} </a>`;
+        ButtonModele4.innerHTML=`<a href="produit.html?ID=${response[3]._id}" type="button" class="btn btn-lg btn-outline-primary">Fiche produit du ${response[3].name} </a>`;
+        ButtonModele5.innerHTML=`<a href="produit.html?ID=${response[4]._id}" type="button" class="btn btn-lg btn-outline-primary">Fiche produit du ${response[4].name} </a>`;
 
-        
+        //TEST
+        console.table(response);
 
-
-
-    };
-}
+         };
+    }
 request.open("GET","http://localhost:3000/api/cameras");
 request.send();
-
-
-//TEST
-//console.log(nom);
-//console.log(prix);
-//console.log(description);
-//console.log(lentilles1);
-
-
