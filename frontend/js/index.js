c475d236aa0bc378f0fb65491c0c64aabad5d6fb
Olivier Barjon <olivier.barjon@gmail.com>
2020-05-15 17:07:12 +0200

    mise en place des boutons et tentative d'ecriture d'un composant de requete api

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -2,44 +2,80 @@
 const appContainer = document.getElementById("app"); // récupération de l'élément "app" du DOM dans une constante "appContainer"
 //console.log(appContainer); // TEST
 
-// 2 : CREATION D'UN COMPOSANT POUR RECUPERER LES OBJETS JSON DANS UN TABLEAU ET LES INTERGRER AU DOM
-class ListProduct { // On crée une classe "ListProduct" qui servira à créer un objet "listproduct"...
+
+// 1.1 : COMPOSANT DE RECUPERATION DE LA LISTE D'OBJETS JSON
+/*class ApiBdd { // création d'une méthode statique pour retourner la liste d'objet JSON dans un tableau
+    constructor(callback) {
+        this.callback = callback;
+    }
+    newRequest() {
+        return xhr = new XMLHttpRequest();
+    }
+    getListProduct () { 
+        var request = this.newRequest();
+        request.onreadystatechange = function() {
+            if(this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+                var response = JSON.parse(this.responseText);}
+            };
+        request.open("GET", "http://localhost:3000/api/cameras", true);
+        request.send();
+    }
+}*/
+
+// 2 : COMPOSANT DE RECUPERATION DES OBJETS JSON DANS UN TABLEAU ET D'INTEGRATION AU DOM
+class ListProducts { // On crée une classe "ListProducts" qui servira à créer un objet "listproducts" (à l'étape 6:) ...
     constructor() {
-        this.products = [ // ... qui  aura une propriété "products" ayant pour valeur la [ liste des objets JSON ]
+         // ... qui  aura une propriété "products" ayant pour valeur la [ liste des objets JSON ]
+        this.products = [
+
             {
-                "lenses": [
-                    "35mm 1.4",
-                    "50mm 1.6"
-                ],
-                "_id": "5be1ed3f1c9d44000030b061",
-                "name": "Zurss 50S",
-                "price": 49900,
-                "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                "imageUrl": "http://localhost:3000/images/vcam_1.jpg"
+            "lenses":["35mm 1.4","50mm 1.6"],
+            "_id":"5be1ed3f1c9d44000030b061",
+            "name":"Zurss 50S",
+            "price":49900,
+            "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            "imageUrl":"http://localhost:3000/images/vcam_1.jpg"
             },
+            
             {
-                "lenses": [
-                    "50mm 1.8",
-                    "60mm 2.8",
-                    "24-60mm 2.8/4.5"
-                ],
-                "_id": "5be1ef211c9d44000030b062",
-                "name": "Hirsch 400DTS",
-                "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                "price": 309900,
-                "imageUrl": "http://localhost:3000/images/vcam_2.jpg"
+            "lenses":["50mm 1.8","60mm 2.8","24-60mm 2.8/4.5"],
+            "_id":"5be1ef211c9d44000030b062",
+            "name":"Hirsch 400DTS",
+            "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            "price":309900,
+            "imageUrl":"http://localhost:3000/images/vcam_2.jpg"
             },
+            
             {
-                "lenses":["25mm 4.5"],
-                "_id":"5be9bc241c9d440000a730e7",
-                "name":"Franck JS 105",
-                "price":209900,
-                "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                "imageUrl":"http://localhost:3000/images/vcam_3.jpg"
-                }
-        ];
-    this.productListComplete = new ProductListView(this.products); // ... qui créera un objet "productListComplete" à partir de la classe "ProductListView" en lui fournissant comme paramètre la [liste des objets JSON]...
-    appContainer.appendChild(this.productListComplete.render()); // ... et qui d'ajoutera à l'élément "app" du DOM, un élément enfant qui sera le retour du rendu (l'objet DOM "productListContainer") de ce nouvel objet "productListView" (étape 5:)
+            "lenses":["25mm 4.5"],
+            "_id":"5be9bc241c9d440000a730e7",
+            "name":"Franck JS 105",
+            "price":209900,
+            "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            "imageUrl":"http://localhost:3000/images/vcam_3.jpg"
+            },
+            
+            {
+            "lenses":["50mm 1.7","35mm 1.4"],
+            "_id":"5be9c4471c9d440000a730e8",
+            "name":"Kuros TTS",
+            "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            "price":159900,
+            "imageUrl":"http://localhost:3000/images/vcam_4.jpg"
+            },
+            
+            {
+            "lenses":["50mm 1.4","35mm 1.8","28-200mm 2.8/4.5"],
+            "_id":"5be9c4c71c9d440000a730e9",
+            "name":"Katatone",
+            "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            "price":59900,
+            "imageUrl":"http://localhost:3000/images/vcam_5.jpg"
+            }
+            
+            ];
+        this.productListComplete = new ProductListView(this.products); // ... qui créera un objet "productListComplete" à partir de la classe "ProductListView" en lui fournissant comme paramètre la [liste des objets JSON]...
+        appContainer.appendChild(this.productListComplete.render()); // ... et qui d'ajoutera à l'élément "app" du DOM, un élément enfant qui sera le retour du rendu (l'objet DOM "productListContainer") de ce nouvel objet "productListView" (étape 5:)
     }
 }
 
@@ -59,17 +95,33 @@ class ListProduct { // On crée une classe "ListProduct" qui servira à créer u
 // 4 : CREATION D'UN COMPOSANT DE VUE POUR GENERER LE RENDU TYPE POUR 1 PRODUIT ( parce que a chaque élément de ma liste je veux générer une vue produit identique)
 
 class ProductView { // On crée une classe qui servira à créer un objet...
-    constructor(product, inList) { // qui prendra deux parametres
-        this.product = product; //... le parametre "product" (qui contient un objet JSON - étape 5:) sera assigné à la 1ère propriété "product".  qui passe un élément "product" de type Product (donc pour être utilisé avec la classe "Product" ci-dessus)
+    constructor(product) { // ...qui prendra un parametre
+        this.product = product; //... le parametre "product" (qui contient un objet JSON - étape 5:) sera assigné à la propriété "product".
     }
     render() { // cette classe a également une méthode de type render...
         const productContainer = document.createElement("div"); // ...qui crée une <div> (en créant un élément du DOM de type <div> qui s'appelle "productContainer") ...
         productContainer.innerHTML = `<div class="card-header"><h3 class="my-0 font-weight-normal">${this.product.name}</h3></div><div class="card-body"><div class="mb-3"><img class="img-fluid" src="${this.product.imageUrl}" alt="${this.product.imageUrl}" /></div><p class="text-justify">${this.product.description}</p><p>Prix : ${this.product.price} €</p></div>`; // ... cette <div> contiendra les éléments HTML remplis par les valeurs de l'objet JSON
+        productContainer.appendChild(new ButtonProductDetailView().render()); // on ajoute à cette <div> notre bouton crée à l'étape 4.1 :
         productContainer.setAttribute("class", "card mb-4 shadow-sm");// on ajoute les styles au modele de conteneur
         return productContainer; // cette méthode d'instance de classe retourne la constante "productContainer" qui contient un modèle de vue de produit opérationnel
     } 
 }
 
+// 4.1 : BOUTON D'ACCES A LA PAGE DE DETAIL
+
+class ButtonProductDetailView { // On crée une classe qui servira à créer un objet...
+    constructor(product) { // ... qui prendra un paramètre
+        this.product = product; //... le parametre "product" (qui contient un objet JSON - étape 5:) sera assigné à la propriété "product".
+    }
+    render () { // cette classe a également une méthode de type render...
+        const buttonLink = document.createElement("a"); //... qui crée un bloc <a> (en créant un élément du DOM de type <a> qui s'appelle "buttonLink")...
+        buttonLink.innerHTML = "Fiche produit"; // l'ancre du lien contiendra un texte simple...
+        buttonLink.setAttribute("class", "btn btn-lg btn-outline-primary"); //... des classes
+        buttonLink.setAttribute("type", "button"); //... un type
+        buttonLink.setAttribute("href", `produit.html?id=`); // ... une adresse de lien
+        return buttonLink; // cette méthode d'instance de classe retourne la constante "buttonLink" qui contient un bouton opérationnel
+    }
+}
 
 // 5 : CREATION D'UN COMPOSANT POUR GENERER LA LISTE COMPLETE DES RENDUS QUI DEVRA ETRE INTEGREE AU DOM
 
@@ -87,8 +139,8 @@ class ProductListView { // On crée une classe qui servira à créer un objet...
     }
 }
 
-/*
-class ApiConsumer {
+
+/*class ApiConsumer {
     constructor({ baseApi }) { // descontruction 
         this.baseApi = baseApi;
     }
@@ -101,12 +153,15 @@ class ApiConsumer {
         request.open("GET", this.baseApi+'/products', true);
         request.send();
     }
-}
-*/
+}*/
+
+
+
+
 
 // 6 : EXECUTION DES COMPOSANTS ET DE LA VUE AU CHARGEMENT DE LA FENÊTRE
 window.onload = function() {  // A l'évènement window.onload (chargement de la fenêtre), on exécute une fonction...
-    const listProduct = new ListProduct(); //...qui crée un nouvel objet "listProduct" à partir de la class "ListProduct" de l'étape 2:
-    console.log(listProduct); //TEST
+    const listProducts = new ListProducts(); //...qui crée un nouvel objet "listProduct" à partir de la class "ListProduct" de l'étape 2:
+    console.log(listProducts); //TEST
 } 
 
